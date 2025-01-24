475207996af53280d502374c4d1184cb60b6a528
Fabien <Thesoigneusse@gmail.com>
2025-01-24 10:40:57 +0100

    modif script multi_enc

--- Scripts/Multi_enc/crt_ctx_full_id.py
+++ Scripts/Multi_enc/crt_ctx_full_id.py
@@ -1,3 +1,7 @@
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+
 from Utils import Utils_data as ud
 from Utils import Utils
 import torch
