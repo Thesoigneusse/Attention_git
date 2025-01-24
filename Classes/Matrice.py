a3890e600047b2ca5f912924959e00f468a94d45
Fabien <Thesoigneusse@gmail.com>
2025-01-24 11:05:18 +0100

    correction

--- Classes/Matrice.py
+++ Classes/Matrice.py
@@ -243,7 +243,7 @@ class Matrice(torch.Tensor):
         # Pour chaque ligne
         for row_idx in range(self.shape[0]):
             # on écrit le token de la phrase + courante + la ligne correspondante de la matrice d'attention
-            to_write += f"{crt.tokens[row_idx]}\t{"\t".join(list(self[row_idx]))}\n" 
+            to_write += f"{crt.tokens[row_idx]}\t{"\t".join(list(self[row_idx]))}\n"
         
         # On écrit le tout dans un fichier tsv
         with open(f"{absolute_folder}/{filename}.tsv", "w") as f:
