3e4e420caa02343a45b4a399ef93272d3c60d439
Fabien <Thesoigneusse@gmail.com>
2025-02-06 12:23:50 +0100

    SC

--- Scripts/Multi_enc/script_marco_full_heads.sh
+++ Scripts/Multi_enc/script_marco_full_heads.sh
@@ -12,7 +12,8 @@ for sentence_level_head in {0..7}; do
         # Modify the parameter
         modified_command=${base_command//list_chemin_0_0.txt/list_chemin_${sentence_level_head}_${token_level_heads}_tsv.txt}
         # Execute the command and store the output in a file
-        output_file="$ATTENTION_GIT/Output/Marco_script_output/output_$sentence_level_head\_$token_level_heads.txt"
+        output_file="$ATTENTION_GIT/Output/Marco_script_output/output_${sentence_level_head}_${token_level_heads}.txt"
+        # output_file= python3 $ATTENTION_GIT/marco_script/match-nmt2parcorfull.py /home/getalp/lopezfab/Attention/k3/GOLD/test.en /home/getalp/lopezfab/Attention/k3/GOLD/test.de $ATTENTION_GIT/marco_script/list_filename_matrice/multi_enc/full_matrice/list_chemin_${sentence_level_head}_${token_level_heads}.txt --output-file=$ATTENTION_GIT/Output/debug.results --canmt-system=multienc --pudb=False
         $modified_command > $output_file
         echo "Output stored in $output_file"
     done
