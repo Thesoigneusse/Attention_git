0f440ff710cb3c065141ac3b05d2d81a2a17249b
Fabien <Thesoigneusse@gmail.com>
2025-02-06 09:23:58 +0100

    small change

--- Scripts/Multi_enc/script_marco_full_heads.sh
+++ Scripts/Multi_enc/script_marco_full_heads.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Define the base command
-HOME = "/home/getalp/lopezfab/lig/Attention_git"
-base_command="python3 $HOME/marco_script/match-nmt2parcorfull.py /home/getalp/lopezfab/lig/Attention/k3/GOLD/test.en /home/getalp/lopezfab/lig/Attention/k3/GOLD/test.de $HOME/marco_script/list_filename_matrice/multi_enc/full_matrice/list_chemin_0_0_tsv.txt --output-file=$HOME/Output/debug.results --canmt-system=multienc --pudb=False"
+ATTENTION_GIT = "/home/getalp/lopezfab/lig/Attention_git"
+base_command="python3 $ATTENTION_GIT/marco_script/match-nmt2parcorfull.py /home/getalp/lopezfab/lig/Attention/k3/GOLD/test.en /home/getalp/lopezfab/lig/Attention/k3/GOLD/test.de $ATTENTION_GIT/marco_script/list_filename_matrice/multi_enc/full_matrice/list_chemin_0_0_tsv.txt --output-file=$ATTENTION_GIT/Output/debug.results --canmt-system=multienc --pudb=False"
 
 # Loop through the desired range
 for sentence_level_head in {0..7}; do
@@ -10,7 +10,7 @@ for sentence_level_head in {0..7}; do
         # Modify the parameter
         modified_command=${base_command//list_chemin_0_0_tsv.txt/list_chemin_${sentence_level_head}_${token_level_heads}_tsv.txt}
         # Execute the command and store the output in a file
-        output_file="$HOME/Output/Marco_script_output/output_${sentence_level_head}_${token_level_heads}.txt"
+        output_file="$ATTENTION_GIT/Output/Marco_script_output/output_${sentence_level_head}_${token_level_heads}.txt"
         $modified_command > $output_file
     done
 done
