94cf6a153af6463641b926effdbaf36ae5d50809
Fabien <Thesoigneusse@gmail.com>
2025-02-06 09:36:27 +0100

    sc

--- Scripts/Multi_enc/script_marco_full_heads.sh
+++ Scripts/Multi_enc/script_marco_full_heads.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the base command
-ATTENTION_GIT = "$HOME/Attention_git"
+ATTENTION_GIT="$HOME/Attention_git"
 base_command="python3 $ATTENTION_GIT/marco_script/match-nmt2parcorfull.py /home/getalp/lopezfab/Attention/k3/GOLD/test.en /home/getalp/lopezfab/Attention/k3/GOLD/test.de $ATTENTION_GIT/marco_script/list_filename_matrice/multi_enc/full_matrice/list_chemin_0_0_tsv.txt --output-file=$ATTENTION_GIT/Output/debug.results --canmt-system=multienc --pudb=False"
 
 # Loop through the desired range
