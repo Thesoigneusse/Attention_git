57c23544a0b21abf9ad979c7ca777bbc6e0b414d
Fabien <Thesoigneusse@gmail.com>
2025-02-06 12:44:26 +0100

    sc

--- Scripts/Multi_enc/script_marco_full_heads.sh
+++ Scripts/Multi_enc/script_marco_full_heads.sh
@@ -10,7 +10,7 @@ for sentence_level_head in {0..7}; do
     for token_level_heads in {0..7}; do
         echo " * Token level head: $token_level_heads"
         # Modify the parameter
-        modified_command=${base_command//list_chemin_0_0.txt/list_chemin_${sentence_level_head}_${token_level_heads}_tsv.txt}
+        modified_command=${base_command//list_chemin_0_0.txt/list_chemin_${sentence_level_head}_${token_level_heads}.txt}
         # Execute the command and store the output in a file
         output_file="$ATTENTION_GIT/Output/Marco_script_output/output_${sentence_level_head}_${token_level_heads}.txt"
         # output_file= python3 $ATTENTION_GIT/marco_script/match-nmt2parcorfull.py /home/getalp/lopezfab/Attention/k3/GOLD/test.en /home/getalp/lopezfab/Attention/k3/GOLD/test.de $ATTENTION_GIT/marco_script/list_filename_matrice/multi_enc/full_matrice/list_chemin_${sentence_level_head}_${token_level_heads}.txt --output-file=$ATTENTION_GIT/Output/debug.results --canmt-system=multienc --pudb=False
