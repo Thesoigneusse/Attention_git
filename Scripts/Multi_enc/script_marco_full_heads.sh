50efc2d612207d7ef51a04abfe723b16ec4796e7
Fabien <Thesoigneusse@gmail.com>
2025-02-06 09:49:07 +0100

    sc

--- Scripts/Multi_enc/script_marco_full_heads.sh
+++ Scripts/Multi_enc/script_marco_full_heads.sh
@@ -6,11 +6,14 @@ base_command="python3 $ATTENTION_GIT/marco_script/match-nmt2parcorfull.py /home/
 
 # Loop through the desired range
 for sentence_level_head in {0..7}; do
+    echo "Sentence level head: $sentence_level_head"
     for token_level_heads in {0..7}; do
+        echo " * Token level head: $token_level_heads"
         # Modify the parameter
         modified_command=${base_command//list_chemin_0_0_tsv.txt/list_chemin_${sentence_level_head}_${token_level_heads}_tsv.txt}
         # Execute the command and store the output in a file
         output_file="$ATTENTION_GIT/Output/Marco_script_output/output_$sentence_level_head_$token_level_heads.txt"
-        $modified_command >> $output_file
+        $modified_command > $output_file
+        echo "Output stored in $output_file"
     done
 done
