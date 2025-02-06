b3424ce213dbbb5a644a38b84074abaa20a0b1a7
Fabien <Thesoigneusse@gmail.com>
2025-02-06 09:17:41 +0100

    Ajout d'un script pour exécuter le script de marco sur l'ensemble des combinaisons de tête sentence x token level. output stocké dans Output/Marco_script_output

---
+++ Scripts/Multi_enc/script_marco_full_heads.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Define the base command
+base_command="python3 ./marco_script/match-nmt2parcorfull.py /home/getalp/lopezfab/lig/Attention/k3/GOLD/test.en /home/getalp/lopezfab/lig/Attention/k3/GOLD/test.de ./marco_script/list_filename_matrice/multi_enc/full_matrice/list_chemin_0_0_tsv.txt --output-file=./Output/debug.results --canmt-system=multienc --pudb=False"
+
+# Loop through the desired range
+for sentence_level_head in {0..7}; do
+    for token_level_heads in {0..7}; do
+        # Modify the parameter
+        modified_command=${base_command//list_chemin_0_0_tsv.txt/list_chemin_${sentence_level_head}_${token_level_heads}_tsv.txt}
+        # Execute the command and store the output in a file
+        output_file="./Output/Marco_script_output/output_${sentence_level_head}_${token_level_heads}.txt"
+        $modified_command > $output_file
+    done
+done
