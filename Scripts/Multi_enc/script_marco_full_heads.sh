a78ea1726856f1e52cc369f8b00250c4bab8ac18
Fabien <Thesoigneusse@gmail.com>
2025-02-06 09:26:02 +0100

    small change

--- Scripts/Multi_enc/script_marco_full_heads.sh
+++ Scripts/Multi_enc/script_marco_full_heads.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Define the base command
-ATTENTION_GIT = "/home/getalp/lopezfab/lig/Attention_git"
-base_command="python3 $ATTENTION_GIT/marco_script/match-nmt2parcorfull.py /home/getalp/lopezfab/lig/Attention/k3/GOLD/test.en /home/getalp/lopezfab/lig/Attention/k3/GOLD/test.de $ATTENTION_GIT/marco_script/list_filename_matrice/multi_enc/full_matrice/list_chemin_0_0_tsv.txt --output-file=$ATTENTION_GIT/Output/debug.results --canmt-system=multienc --pudb=False"
+ATTENTION_GIT = "$HOME/Attention_git"
+base_command="python3 $ATTENTION_GIT/marco_script/match-nmt2parcorfull.py /home/getalp/lopezfab/Attention/k3/GOLD/test.en /home/getalp/lopezfab/Attention/k3/GOLD/test.de $ATTENTION_GIT/marco_script/list_filename_matrice/multi_enc/full_matrice/list_chemin_0_0_tsv.txt --output-file=$ATTENTION_GIT/Output/debug.results --canmt-system=multienc --pudb=False"
 
 # Loop through the desired range
 for sentence_level_head in {0..7}; do
