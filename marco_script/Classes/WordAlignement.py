59a2e57ee6e2d1a7583bddd76399ef1bc80110d4
Fabien <Thesoigneusse@gmail.com>
2025-02-12 10:14:58 +0100

    Changement de WordAlignement.py en dataclass

--- marco_script/Classes/WordAlignement.py
+++ marco_script/Classes/WordAlignement.py
@@ -1,6 +1,9 @@
+from dataclasses import dataclass, field
+from typing import Literal
 import sys
 sys.path.append('/home/getalp/lopezfab/Bureau/Attention_git/marco_script')
 
+@dataclass
 class WordAlignement():
     """Objet representant un alignement entre un mot de reference et un mot hypothese. 
     Specifie:
@@ -8,40 +11,35 @@ class WordAlignement():
     - l'index du mot dans la phrase de reference
     - l'index du mot dans la phrase hypothese.
     """
+    alignement_type: Literal['del', 'ins', 'sub', 'match']
+    index_reference: int
+    index_hypothese: int
     
     def __init__(self, alignement_type: str, index_reference: int, index_hypothese: int):
         self.alignement_type = alignement_type
         self.index_reference = index_reference
         self.index_hypothese = index_hypothese
 
-    def __str__(self):
-        return f"Alignement(alignement_type={self.alignement_type}, index_reference={self.index_reference}, index_hypothese={self.index_hypothese})"
+    def __post_init__(self):
+        """On restreint les valeurs possibles de alignement_type à 'del', 'ins', 'sub', 'match'
 
-    def __repr__(self):
-        return str(self)
+        Raises:
+            ValueError: _description_
+        """
+        alignement_type_valeurs_valides = {'del', 'ins', 'sub', 'match'}
+        if self.alignement_type not in alignement_type_valeurs_valides:
+            raise ValueError(f"alignement_type invalide : {self.alignement_type}. Possible values: {alignement_type_valeurs_valides}")
 
-    @property
-    def alignement_type(self):
-        return self._alignement_type
-    @alignement_type.setter
-    def alignement_type(self, alignement_type):
-        assert alignement_type in ['ins', 'del', 'sub', 'match'], f"[DEBUG] alignement_type must be in ['ins', 'del', 'sub', 'match']. Current alignement_type: {alignement_type}"
-        self._alignement_type = alignement_type
+    # def __str__(self):
+    #     return f"Alignement(alignement_type={self.alignement_type}, index_reference={self.index_reference}, index_hypothese={self.index_hypothese})"
+    
+    # def __repr__(self):
+    #     return str(self)
 
-    @property
-    def index_reference(self):
-        return self._index_reference
-    @index_reference.setter
-    def index_reference(self, index_reference):
-        self._index_reference = index_reference
 
-    @property
-    def index_hypothese(self):
-        return self._index_hypothese
-    @index_hypothese.setter
-    def index_hypothese(self, index_hypothese):
-        self._index_hypothese = index_hypothese
+
 
 if __name__ == "__main__":
     alignement = WordAlignement("ins", 1, 2)
     print(alignement)
+
