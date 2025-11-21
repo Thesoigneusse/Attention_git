90d5993366a864b7a9888447a729425781c8a4a6
Fabien <Thesoigneusse@gmail.com>
2025-11-21 09:56:44 +0100

    Ajout de script afinde lancer la commande python avec des paramètres fixes

--- Scripts/Multi_enc/script_marco_one_head.sh
+++ Scripts/Multi_enc/script_marco_one_head.sh
@@ -2,30 +2,24 @@
 
 # Define the base command
 ATTENTION_GIT="$HOME/Attention_git"
-
 SCRIPT="$ATTENTION_GIT/marco_script/match-nmt2parcorfull.py"
 GOLD_EN="$HOME/Attention/k3/GOLD/test.en"
 GOLD_DE="$HOME/Attention/k3/GOLD/test.de"
-LISTE_CHEMIN_MATRICE="$ATTENTION_GIT/marco_script/list_filename_matrice/concat/full_matrice/list_chemin_0_0.txt"
-CANMT_SYSTEM="concat"
+LISTE_CHEMIN_MATRICE="$ATTENTION_GIT/marco_script/list_filename_matrice/multi_enc/full_matrice/list_chemin_0_0.txt"
+OUTPUT_FILE="$ATTENTION_GIT/Output/debug.results_2"
+CANMT_SYSTEM="multienc"
 
-OUTPUT_FILE="$ATTENTION_GIT/Output/debug.results_v3"
-
-base_command="python3 $SCRIPT $GOLD_EN $GOLD_DE $LISTE_CHEMIN_MATRICE --output-file=$OUTPUT_FILE --canmt-system=$CANMT_SYSTEM --pudb=False --local=False"
-
-# python3 ./marco_script/match-nmt2parcorfull.py /home/getalp/lopezfab/Attention/k3/GOLD/test.en /home/getalp/lopezfab/Attention/k3/GOLD/test.de ./marco_script/list_filename_matrice/concat/full_matrice/list_chemin_0_0.txt --output-file=./Output/concat_debug.results --canmt-system=concat --pudb=False
+base_command="python3 $SCRIPT $GOLD_EN $GOLD_DE $LISTE_CHEMIN_MATRICE --output-file=$OUTPUT_FILE --canmt-system=$CANMT_SYSTEM --pudb=False"
 
 # Loop through the desired range
-sentence_level_head=1
-token_level_heads=3
+sentence_level_head=0
 echo "Sentence level head: $sentence_level_head"
+token_level_heads=7
 echo " * Token level head: $token_level_heads"
-
 # Modify the parameter
 modified_command=${base_command//list_chemin_0_0.txt/list_chemin_${sentence_level_head}_${token_level_heads}.txt}
-
 # Execute the command and store the output in a file
-output_file="$ATTENTION_GIT/Output/Marco_script_output/concat/output_${sentence_level_head}_${token_level_heads}_v2.txt"
+output_file="$ATTENTION_GIT/Output/Marco_script_output/multi_enc/V2/output_${sentence_level_head}_${token_level_heads}.txt"
 # output_file= python3 $ATTENTION_GIT/marco_script/match-nmt2parcorfull.py /home/getalp/lopezfab/Attention/k3/GOLD/test.en /home/getalp/lopezfab/Attention/k3/GOLD/test.de $ATTENTION_GIT/marco_script/list_filename_matrice/multi_enc/full_matrice/list_chemin_${sentence_level_head}_${token_level_heads}.txt --output-file=$ATTENTION_GIT/Output/debug.results --canmt-system=multienc --pudb=False
 $modified_command > $output_file
 echo "Output stored in $output_file"
